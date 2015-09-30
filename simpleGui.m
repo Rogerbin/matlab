512fa17766e35f5b5e1b0a363309f99621363e94
Rogerbin <viomab@sina.com>
2015-09-30 11:09:53 +0800

    Matlab GUI with only m file
    not using guide

---
+++ simpleGui.m
@@ -0,0 +1,34 @@
+function simpleGui
+%pcode simpleGui
+h.fig = figure('position',[600 350 210 400],'menu','none');
+
+h.buttonone = uicontrol('style','pushbutton',...
+                    'position',[10 360 100 40],...
+                    'string','btn1');
+                
+set(h.buttonone,'callback',{@addbutton , h});
+h.btnMenu = uicontrol('style','pushbutton',...
+                       'position',[10, 300, 100,40],...
+                       'string','menu');
+set(h.btnMenu,'callback',{@showmenu,h});
+
+function h = addbutton(hObject, eventdata,h)
+
+h.buttonTwo = uicontrol('style','pushbutton',...
+                        'position',[100 360 100 40],...
+                        'string','remove button');
+set(h.buttonTwo, 'callback',{@removebutton,h});
+set(h.buttonone,'enable','off');
+
+function h = removebutton(hObject, eventdata, h)
+
+delete(h.buttonTwo);
+h = rmfield(h,'buttonTwo');
+
+set(h.buttonone,'enable','on');
+
+function h = showmenu(hObject, eventdata, h)
+
+if isequal(get(h.fig,'menu'),'none')
+    set(h.fig,'MenubBar','auto');
+end
